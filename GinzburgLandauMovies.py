5a88244436d10a43df2e2dc9c70f6a31dfb96264
Hannes Vandecasteele <hannesvdc@gmail.com>
2024-06-26 11:14:24 -0400

    Made first version of histogram figure.

--- GinzburgLandauMovies.py
+++ GinzburgLandauMovies.py
@@ -32,7 +32,7 @@ def _parameters(filename):
     eta = float(filename[index4+5:index5])
     return {'c1': c1, 'c2': c2, 'nu': nu, 'eta': eta}
 
-def _load_data(data_directory, params):
+def _load_data(data_directory, params, reduce=False):
     min_A = np.inf
     max_A = 0
     data = []
@@ -44,7 +44,7 @@ def _load_data(data_directory, params):
         if _parameters(filename.name) != params:
             continue
         T = _T(filename.name)
-        if int(T) % 5 != 0:
+        if reduce and int(T) % 5 != 0:
             continue
 
         print(filename.name)
@@ -124,7 +124,7 @@ def make3DPlots(data_directory, video_directory):
     params = {'c1': 0.2, 'c2': 0.61, 'nu': 1.5, 'eta': 1.0}
     M = 512
 
-    data, min_A, max_A = _load_data(data_directory, params)
+    data, min_A, max_A = _load_data(data_directory, params, reduce=True)
     print('min/max mod', min_A, max_A)
 
     # Do actual plotting
@@ -180,52 +180,55 @@ def make3DMovie(directory):
     cv2.destroyAllWindows()
     video.release()
 
-def makeHistogramMovie(directory=None):
-    data = []
-    if directory is None:
-        directory = '/Users/hannesvdc/Research_Data/emergent/Ginzburg_Landau_New_Images/'
-    min_mod = 2.0
-    max_mod = 0.0
-    for filename in os.scandir(directory):
-        if not filename.is_file() or not filename.name.endswith('.npy'):
-            continue
-        T = _T(filename.name)
-        W = np.load(directory + filename.name)
-        data.append((T, np.absolute(W).flatten(), np.angle(W).flatten() + np.pi))
-        min_mod = min(min_mod, np.min(np.absolute(W)))
-        max_mod = max(max_mod, np.max(np.absolute(W)))
-    print(min_mod, max_mod)
-    data.sort()
+def makeHistogramMovie(directory):
+    params = {'c1': 0.2, 'c2': 0.61, 'nu': 1.5, 'eta': 1.0}
+    M = 512
+    dx = 1.0 / M
 
-    threshold = 1500
-    kernel_size = 30
-    kernel = np.ones((kernel_size, kernel_size)) / kernel_size**2
+    # Load data and Compute D = W_xx + W_yy
+    data, min_mod, max_mod = _load_data(directory, params)
+    D_data = list()
+    min_angle, max_angle = -np.pi, np.pi
+    for (T, W) in data:
+        Wxx = (np.roll(W, -1, axis=0) - 2.0*W + np.roll(W, 1, axis=0))
+        Wyy = (np.roll(W, -1, axis=1) - 2.0*W + np.roll(W, 1, axis=1))
+        D = (Wxx + Wyy) / dx**2
+
+        D_angle = np.angle(D)
+        min_angle = min(min_angle, np.min(D_angle))
+        max_angle = max(max_angle, np.max(D_angle))
+        D_data.append((T, D_angle))
+
     bins = [100, 100]
-    edges = [[min_mod, max_mod], [0.0, 2.0*np.pi]]
+    edges = [[min_mod, max_mod], [min_angle, max_angle]]
     x_grid = np.linspace(min_mod, max_mod, bins[0])
-    y_grid = np.linspace(0.0, 2.0*np.pi, bins[1])
+    y_grid = np.linspace(min_angle, max_angle, bins[1])
     X2, Y2 = np.meshgrid(x_grid, y_grid)
-    for element in data:
-        print('T =', element[0], np.min(element[2]), np.max(element[2]), np.mean(element[2]))
-        H, x_edges, y_edges = np.histogram2d(x=element[1], y=element[2], bins=bins, range=edges)
-        print(np.min(H),np.max(H))
-        print(H.shape, kernel.shape)
-        H = scipy.signal.convolve2d(H, kernel, mode='same', boundary='wrap')
-        H[np.where(H>threshold)] = threshold
-        H = H / np.sum(H)
+    for index in range(len(data)):
+        print('T = ', data[index][0])
+        T = data[index][0]
+        W = data[index][1]
+        D = D_data[index][1]
+
+        H, _, _ = np.histogram2d(np.absolute(W).flatten(), D.flatten(), bins=bins, range=edges)
+        H = scipy.ndimage.gaussian_filter(H, axes=0, sigma=0.2, mode='wrap')
+        H[0:90, :] = 50.0 * H[0:90, :] # Scale for visualization, does not scale underlying data
+
 
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
-        ax.plot_surface(X2, Y2, H.T, rstride=5, cstride=5, color='orangered', edgecolors='k', lw=0.6)
-        ax.set_xlabel(r'$|W|$')
-        ax.set_ylabel(r'$\angle W$')
-        ax.set_title(r'$T = $'+str(element[0]))
-        plt.savefig(directory + 'GL_Swarm_T=' + dot_to_bar(str(round(element[0],4))) + '.png')
+        ax.plot_surface(Y2, X2, H.T, color='gray', alpha=0.6)
+        ax.set_xlabel(r'$\angle \Delta W$')
+        ax.set_ylabel(r'$|W|$')
+        ax.set_zlabel(r'$g$')
+        ax.set_title(r'$T = $'+str(T))
+        ax.set_zlim(-10000, 25000)
+        plt.savefig(directory + 'GL_Histogram_T=' + dot_to_bar(str(round(T,4))) + '.png')
         plt.close()
 
     images = []
     for img in os.listdir(directory):
-        if not img.endswith('.png') or not img.startswith('GL_Swarm'):
+        if not img.endswith('.png') or not img.startswith('GL_Histogram'):
             continue
         T = _T(bar_to_dot(img), ext='.png')
         images.append((T, img))
@@ -233,8 +236,8 @@ def makeHistogramMovie(directory=None):
         
     frame = cv2.imread(os.path.join(directory, images[0][1]))
     height, width, _ = frame.shape
-    video_name = 'Ginzburg_Landau_Swarm.avi'
-    fps = 10
+    video_name = 'Ginzburg_Landau_Histogram.avi'
+    fps = 5
     video = cv2.VideoWriter(directory + video_name, 0, fps, (width,height))
 
     for image in images:
@@ -242,6 +245,27 @@ def makeHistogramMovie(directory=None):
 
     cv2.destroyAllWindows()
     video.release()
+
+def makeSwarmPlots(data_directory):
+    # Load simulation data into memory
+    params = {'c1': 0.2, 'c2': 0.61, 'nu': 1.5, 'eta': 1.0}
+    data, _, _ = _load_data(data_directory, params)
+
+    # Compute finite differences approximation of the gradient for all T
+    M = 512
+    assert data[0][1].shape[0] == M
+    dx = 1.0 / M
+    for (T, W) in data:
+        Wx = (np.roll(W, -1, axis=0) - np.roll(W, 1, axis=0)) / (2.0*dx)
+        Wy = (np.roll(W, -1, axis=1) - np.roll(W, 1, axis=1)) / (2.0*dx)
+        D = Wx + Wy
+
+        RD = np.real(D)
+        ID = np.imag(D)
+        print('T =', T, np.min(RD), np.max(RD), np.min(ID), np.max(ID))
+
+    
+
 
 if __name__ == '__main__':
     def parseArguments():
@@ -259,15 +283,18 @@ if __name__ == '__main__':
     args = parseArguments()
     
     if args.run_type == '3d_video':
-        directory = '/Users/hannesvdc/Research_Data/emergent/Ginzburg_Landau_Circular/'
+        directory = '/Users/hannesvdc/Research_Data/emergent/Ginzburg_Landau_3D/'
         make3DPlots(directory, directory)
         make3DMovie(directory)
     elif args.run_type == '2d_video':
-        directory = '/Users/hannesvdc/Research_Data/emergent/Ginzburg_Landau_Swarm/'
+        directory = '/Users/hannesvdc/Research_Data/emergent/Ginzburg_Landau_Simulation/'
         make2DPlots(directory=directory)
         make2DMovie(image_folder=directory)
     elif args.run_type == 'histogram_video':
-        directory = '/Users/hannesvdc/Research_Data/emergent/Ginzburg_Landau_Swarm/'
+        directory = '/Users/hannesvdc/Research_Data/emergent/Ginzburg_Landau_3D/'
         makeHistogramMovie(directory=directory)
+    elif args.run_type == 'swarm_movie':
+        directory = '/Users/hannesvdc/Research_Data/emergent/Ginzburg_Landau_3D/'
+        makeSwarmPlots(directory)
     else:
         print('Type of experiment not recognized. Returning.')
